51345ffceadd33c15212d889039644f9c56a3f1f
Joshua Tan <tantohyung@gmail.com>
2016-09-07 12:36:49 +0000

    Add test to static pages controller

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe StaticPagesController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
   describe '#landing_page' do
     it 'should display landing page' do
-      user
+      get :landing_page
+      expect(response).to have_http_status(:success)
     end
   end
 end
