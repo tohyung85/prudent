8312570e18a8d8de32f86cfb67805f596828221b
Joshua Tan <tantohyung@gmail.com>
2016-09-09 11:15:31 +0000

    Fix rubocop errors

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def landing_page
-    render(:layout => "layouts/no_footer_layout")
+    render(layout: 'layouts/no_footer_layout')
   end
 end
