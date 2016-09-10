fb5b88d459b512b0494c6586adf0698d5686e389
Joshua Tan <tantohyung@gmail.com>
2016-09-10 00:44:10 +0000

    Add Category Model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,23 @@
 FactoryGirl.define do
-  factory :project do
-    
+  factory :category do
+    name "Retail"  
   end
+
+  factory :project do
+    sequence :name do |n|
+      "Project#{n}"
+    end
+    location "A nice place"
+    size 123
+    published? true
+
+    factory :unpublished_project do
+      published? true
+    end
+    association :category
+  end
+
+  
   factory :user do
     sequence :email do |n|
       "dummyEmail#{n}@gmail.com"
