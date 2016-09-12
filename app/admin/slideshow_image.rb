9e7db616e387dd35ec46dd6cb1f338262e30b115
Joshua Tan <tantohyung@gmail.com>
2016-09-12 01:26:21 +0000

    Set up backend for slideshow images

---
+++ app/admin/slideshow_image.rb
@@ -0,0 +1,36 @@
+ActiveAdmin.register SlideshowImage do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  permit_params :photo, :row_order, :description
+
+  index do
+    column :photo do |slideshow_image|
+      link_to admin_slideshow_image_path(slideshow_image) do
+        image_tag slideshow_image.photo.thumb.url, class: 'active-admin-thumbnail'
+      end
+    end
+    column :description
+    column :row_order
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :photo do |slideshow_image|
+        image_tag slideshow_image.photo.medium.url, class: 'active-admin-main-image'
+      end
+      row :description
+      row :row_order
+    end
+  end
+end
---
+++ spec/models/slideshow_image_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe SlideshowImage, type: :model do
+end
